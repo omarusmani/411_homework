af6164df7ea3e6d74055e172b504257d67bd1885
omarusmani <omarusmani3@hotmail.com>
2022-09-21 22:48:33 -0500

    added delete button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
+class App extends React.Component {
+
+  constructor(props){
+    super(props)
+
+    this.state= {
+      isClicked:false,
+      todos: [],
+      text: ""
+    }
+    this.handleClick=this.handleClick.bind(this)
+  }
+  handleClick(){
+    console.log("Click Handled")
+    console.log(this.state.isClicked)
+    // if(this.isClicked===true){
+    //   this.setState({isClicked:true});
+    // }else{
+    //   this.setState({isClicked: !this.state.isClicked})
+    // }
+    this.setState({isClicked:!this.state.isClicked})
+  }
+  handleChange=(event)=>{
+    // console.log(event.target.value)
+    this.setState({
+      text: event.target.value
+    })
+  }
+  handleSubmit=()=>{
+    // console.log(event.target.value)
+    this.setState({
+      todos:[...this.state.todos, this.state.text],
+      text:""
+    })
+    
+  }
+  handleDelete=(index)=>{
+      let x=[...this.state.todos]
+      x.splice(index,1)
+    // console.log(event.target.value)
+    this.setState({
+      todos:[...x],
+    
+    })
+    
+  }
+componentDidUpdate(){
+
+  // console.log(this.state.text)
+  console.log(this.state.todos)
+
+}
+
+  render(){
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>HEllo</h1>
+      <h2>ToDos LIST</h2>
+      <input type="text" onChange={this.handleChange} value={this.state.text}/>
+      {/* <button onClick={this.handleClick}>{this.state.isClicked===true ? "Toggle" : "UnToggle"}</button> */}
+      <button onClick={this.handleSubmit}>Submit</button>
+      <ul>
+      {
+        this.state.todos.map((todo,index)=>{
+          return<li key={index}>{todo} <button onClick={(e)=>this.handleDelete({index})}>Delete</button>
+          </li>
+        })
+      }</ul>
     </div>
-  );
+      )}
 }
 
 export default App;
